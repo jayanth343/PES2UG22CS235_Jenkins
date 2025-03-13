fdc63474b36d110c8abeeb2020680970cb8b1733
jayanth343 <jayanth.srinivasan99@gmail.com>
2025-03-13 15:59:33 +0530

    update hello.cpp 2.0

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World from PES2UG22CS235!\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this app. ;) \n";
   return 0;
 }
